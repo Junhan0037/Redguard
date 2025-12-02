ddd2ed9b1600053468c7b62ec66fc5f91d948ed8
Junhan0037 <wnsgks0037@naver.com>
2025-12-02 13:48:23 +0900

    feat: 프로파일별 로그 레벨/샘플링 정책 정의 및 모니터링 시스템 연계

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    implementation("io.micrometer:micrometer-registry-prometheus")
 
     runtimeOnly("org.postgresql:postgresql")
 
